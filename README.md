620cb523f2bb7f256a1f5cc00e28965ecb5c700d
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-10-05 21:07:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Automated PDF Bookmarking Tool 🔖
+
+This is a tool to automatically generate bookmarks for PDF files given a formatted list of bookmarks in plain text.
+
+## Intended Usage ✍️
+
+Designed to add bookmarks to an AP US History supplemental reading, but can be used for any PDF file with customization
+
+Sample input file is included in the repository (in.txt). The input file is automatically nested based on the configuration of
+the specific book this code was built to target.
+
+## Technologies Used ⚙
+
+* Python 3.7
+* PyPDF2
---
+++ main.py
@@ -0,0 +1,73 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+OFFSET = 38
+
+
+def get_bookmark_data():
+    bookmarks = []
+    with open('in.txt', 'r', encoding='utf-8') as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            if line == '':
+                continue
+
+            top_level = False
+            if line.startswith('$ '):
+                line = line.replace('$ ', '')
+                top_level = True
+
+            page_number = line.split(' ')[-1]
+            title = line[:-len(page_number) - 1]
+
+            if title.startswith('PERIOD') or top_level:
+                parent = bookmarks
+            else:
+                if title.startswith('Chapter') or title.startswith('Period'):
+                    parent = bookmarks[-1]['children']
+                else:
+                    parent = bookmarks[-1]['children'][-1]['children']
+
+            parent.append({
+                'title': title,
+                'page': int(page_number),
+                'children': []
+            })
+    return bookmarks
+
+
+def add_bookmarks(bookmarks, parent=None):
+    for bookmark_to_create in bookmarks:
+        bookmark = writer.addBookmark(
+            bookmark_to_create['title'], bookmark_to_create['page'] + OFFSET - 1, parent=parent)
+        add_bookmarks(bookmark_to_create['children'], parent=bookmark)
+
+
+if __name__ == '__main__':
+    b = get_bookmark_data()
+    print('Finished reading bookmarks')
+    reader = PdfFileReader("AMSCO 2016.pdf")  # open input
+    writer = PdfFileWriter()  # open output
+
+    for page in reader.pages:
+        writer.addPage(page)
+
+    writer.add_metadata({
+        '/Title': 'AMSCO 2016',
+        '/Author': 'John Newman and John Schmalbach',
+        '/Subject': 'AP United States History',
+        '/Keywords': 'AMSCO AP United States History 2016',
+        '/Creator': 'AMSCO',
+        '/Producer': 'AMSCO School Publications, Inc.',
+        '/CreationDate': 'D:20161001000000Z',
+        '/ModDate': 'D:20161001000000Z',
+        '/Trapped': '/False'
+    })
+
+    print('Finished copying pages')
+
+    add_bookmarks(b)
+    print('Finished adding bookmarks')
+
+    with open("result.pdf", "wb") as fp:  # creating result pdf JCT
+        writer.write(fp)  # writing to result pdf JCT
+    print('Finished writing to file')
